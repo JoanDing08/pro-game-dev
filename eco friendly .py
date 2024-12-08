6725a4ebfc5868366b59b0a495372c76cf6703d5
JoanDing08 <j.okia.ding08@gmail.com>
2024-12-08 15:55:47 +0000

    Update eco friendly .py

--- eco friendly .py
+++ eco friendly .py
@@ -11,14 +11,18 @@ class bin(pygame.sprite.Sprite):
     super().__init__()
     self.image=pygame.transform.scale(pygame.image.load("bin.png"),(50,50))
     self.rect=self.image.get_rect()
+  def update():
+    pos=pygame.mouse.get_pos()
 
 class recyclable(pygame.sprite.Sprite):
   def __init__(self,img):
+    pygame.sprite.Sprite.__init__(self)
     self.image=pygame.transform.scale(pygame.image.load(img),(50,50))
     self.rect=self.image.get_rect()
 
 class nonrecyclable(pygame.sprite.Sprite):
   def __init__(self):
+    pygame.sprite.Sprite.__init__(self)
     self.image=pygame.transform.scale(pygame.image.load("bag.png"),(50,50))
     self.rect=self.image.get_rect()
 
@@ -29,16 +33,21 @@ nonrecyclables=pygame.sprite.Group()
 player=bin()
 allsprites.add(player)
 
-recycle=["paper bag.png","pencil","crate.png"]
+recycle=["paper bag.png","pencil.png","crate.png"]
 
-for i in range(50):
+for i in range(40):
   item=recyclable(random.choice(recycle))
-  item.rect.x=(random.randint(50,650))
-  item.rect.y=(random.randint(50,650))
+  item.rect.x=(random.randint(30,770))
+  item.rect.y=(random.randint(30,670))
   allsprites.add(item)
   recyclables.add(item)
 
-
+for i in range(10):
+  trash=nonrecyclable()
+  trash.rect.x=(random.randint(30,770))
+  trash.rect.y=(random.randint(30,670))
+  allsprites.add(trash)
+  nonrecyclables.add(trash)
 
 while True:
   for event in pygame.event.get():
