7442e924c0d713854c2410f10b04d944c71dbe0b
JoanDing08 <j.okia.ding08@gmail.com>
2024-12-09 16:49:05 +0000

    Update eco friendly .py

--- eco friendly .py
+++ eco friendly .py
@@ -5,14 +5,20 @@ pygame.init()
 
 pygame.display.set_caption("Eco-Friendly")
 screen=pygame.display.set_mode((800,700))
+bg=pygame.transform.scale(pygame.image.load("eco friendly bg.png"),(800,700))
+startTime=time.time()
+score=0
+minScore=10
+
+font=pygame.font.SysFont("Calibri",40)
+scoreTxt=font.render("Score: "+str(score),True,"white")
 
 class bin(pygame.sprite.Sprite):
   def __init__(self):
     super().__init__()
     self.image=pygame.transform.scale(pygame.image.load("bin.png"),(50,50))
     self.rect=self.image.get_rect()
-  def update():
-    pos=pygame.mouse.get_pos()
+
 
 class recyclable(pygame.sprite.Sprite):
   def __init__(self,img):
@@ -35,17 +41,17 @@ allsprites.add(player)
 
 recycle=["paper bag.png","pencil.png","crate.png"]
 
-for i in range(40):
+for i in range(20):
   item=recyclable(random.choice(recycle))
-  item.rect.x=(random.randint(30,770))
-  item.rect.y=(random.randint(30,670))
+  item.rect.x=(random.randint(30,750))
+  item.rect.y=(random.randint(30,650))
   allsprites.add(item)
   recyclables.add(item)
 
-for i in range(10):
+for i in range(5):
   trash=nonrecyclable()
-  trash.rect.x=(random.randint(30,770))
-  trash.rect.y=(random.randint(30,670))
+  trash.rect.x=(random.randint(30,750))
+  trash.rect.y=(random.randint(30,650))
   allsprites.add(trash)
   nonrecyclables.add(trash)
 
@@ -53,5 +59,41 @@ while True:
   for event in pygame.event.get():
     if event.type==pygame.QUIT:
       exit()
-  allsprites.draw(screen)
+    
+  timeElapsed=time.time()-startTime
+  countDown=font.render(str(timeElapsed),True,"white")
+
+  if timeElapsed>=25:
+    if score>=minScore:
+      screen.fill("green")
+      success=font.render(f"Time's up. You got {score}/20. Great job.",True,"white")
+      screen.blit(success,(10,350))
+    else:
+      screen.fill("red")
+      fail=font.render(f"Time's up. You got {score}/20. Better luck next time.",True,"white")
+      screen.blit(fail,(10,350))
+  else:
+    screen.blit(bg,(0,0))
+    key=pygame.key.get_pressed()
+    if key[pygame.K_UP] and player.rect.y>0:
+      player.rect.y-=6
+    if key[pygame.K_DOWN] and player.rect.y<800:
+      player.rect.y+=6
+    if key[pygame.K_LEFT] and player.rect.x>0:
+      player.rect.x-=6
+    if key[pygame.K_RIGHT] and player.rect.x<700:
+      player.rect.x+=6
+    
+    r_list=pygame.sprite.spritecollide(player,recyclables,True)
+    nr_list=pygame.sprite.spritecollide(player,nonrecyclables,True)
+    for i in r_list:
+      score+=1
+      scoreTxt=font.render("Score: "+str(score),True,"white")
+    for i in nr_list:
+      score-=2
+      scoreTxt=font.render("Score: "+str(score),True,"white")
+    screen.blit(scoreTxt,(10,10))
+    screen.blit(countDown,(770,670))
+
+    allsprites.draw(screen)
   pygame.display.update()
